58d541f5051b13b641a917c43e82728feb810f86
ExtraStrawberryMilk <strawberrymilk.strawberries@gmail.com>
2023-12-02 15:22:59 +1300

    This is the final verision for submission :D

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,27 +4,28 @@ var blue, yellow, pink, brown, darkGreen, green;
 //parameters [BB = blueberry] [PA = pineapple]
 const BBOnePosition = [80, 110];
 const BBTwoPosition = [170, 180];
-const PAOnePosition = [150, 60];
+const PAOnePosition = [150, 60]; 
 const crossStrokeWeight = 5;
-const squareSize = 40;
+const squareSize = 90;
 const BBSize = 30;
-const PASize = 60;
+const PASize = 100;
 const PAAngle = 20;
-const BBAngle = -20;
+const BBAngle = 140;
+const drawBlueberry = false;
 
 /*
  *
  */
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(GLIDE_WALLPAPER);
   //pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset = 65;
+  pWallpaper.grid_settings.row_offset = 30;
 
   //setting up colours
   blue = color(39, 94, 172);
@@ -63,9 +64,13 @@ function my_symbol() {
   line(50, 150, 20, 180);
 
   //displaying the two blueberries and pineapple
-  display(BBOnePosition[0], BBOnePosition[1], BBAngle, new Blueberry(BBSize));
-  display(BBTwoPosition[0], BBTwoPosition[1], BBAngle, new Blueberry(BBSize));
-  display(PAOnePosition[0], PAOnePosition[1], PAAngle, new Pineapple(PASize));
+  
+    display(PAOnePosition[0], PAOnePosition[1], PAAngle, new Pineapple(PASize));
+    if(drawBlueberry){
+    display(BBOnePosition[0], BBOnePosition[1], BBAngle, new Blueberry(BBSize));
+    display(BBTwoPosition[0], BBTwoPosition[1], BBAngle, new Blueberry(BBSize));
+    }
+  
 }
 
 /*
@@ -90,61 +95,47 @@ class Pineapple
     this.size = size;
   }
 
+  /* draws a leaf for the pineapple
+  * Inputs: col (color), x (float), y (float), angle (float)
+  * Return: Void
+  */
+  #drawLeaf(col, x, y, angle){
+    fill(col);
+    push();
+    translate(x,y);
+    rotate(angle);
+    ellipse(0, 0, this.size/6, this.size/2.4);
+    pop();
+  }
+
   show() 
   {
     stroke(brown);
     strokeWeight(0.5);
 
+    //draws body
     fill(yellow);
     ellipse(0, 0, this.size / 1.5, this.size / 1.2);
 
+    //draws eyes
     fill(0);
-    circle(-8, 0, 6);
-    circle(8, 0, 6);
+    circle(this.size/-7.5, 0, this.size/10);
+    circle(this.size/7.5, 0, this.size/10);
 
-    arc(0, 5, this.size / 8, this.size / 8, 0, 180, CHORD);
+    //draws mouth
+    arc(0, this.size/12, this.size / 8, this.size / 8, 0, 180, CHORD);
 
+    //draws pupils
     fill(255);
-    circle(-9, -1, 2);
-    circle(7, -1, 2);
+    circle(this.size/-6.66, this.size/-60, this.size/30);
+    circle(this.size/8.57, this.size/-60, this.size/30);
 
-    fill(green);
-
-    push();
-    translate(-9, -28);
-    rotate(-70);
-    ellipse(0, 0, 10, 25);
-    pop();
-
-    push();
-    translate(9, -28);
-    rotate(70);
-    ellipse(0, 0, 10, 25);
-    pop();
-
-    fill(darkGreen);
-    push();
-    translate(-6, -31);
-    rotate(140);
-    ellipse(0, 0, 10, 25);
-    pop();
-
-    push();
-    translate(6, -31);
-    rotate(50);
-    ellipse(0, 0, 10, 25);
-    pop();
-
-    fill(green);
-    push();
-    translate(0, -33);
-    ellipse(0, 0, 10, 25);
-    pop();
-
-
-
-
-  }
+    this.#drawLeaf(green, this.size/-6.66, this.size/-2.14, -70);
+    this.#drawLeaf(green, this.size/6.66, this.size/-2.14, 70);
+    this.#drawLeaf(darkGreen, this.size/-10, this.size/-1.93, 140);
+    this.#drawLeaf(darkGreen, this.size/10, this.size/-1.82, 50);
+    this.#drawLeaf(green, 0, this.size/-1.8, 0);
+}
 }
 
 /*
@@ -158,31 +149,39 @@ class Blueberry {
   show() {
     stroke(brown);
     strokeWeight(0.5);
+
+    //body
     fill(blue);
-    circle(0, 0, this.size);
+    circle(0, 0, this.size); 
     fill(15, 61, 121);
+    
+    //top of the blueberry
     beginShape();
-    vertex(10, -2 - this.size / 2);
-    vertex(5, -this.size / 2);
-    vertex(0, -2 - this.size / 2);
-    vertex(-5, -this.size / 2);
-    vertex(-10, -2 - this.size / 2);
-    vertex(-10, 4 - this.size / 2);
-    vertex(10, 4 - this.size / 2);
+    vertex(this.size/3, this.size/-15 - this.size / 2);
+    vertex(this.size/6, -this.size / 2);
+    vertex(0, this.size/-15 - this.size / 2);
+    vertex(this.size/-6, -this.size / 2);
+    vertex(this.size/-3, this.size/-15 - this.size / 2);
+    vertex(this.size/-3, this.size/7.5 - this.size / 2);
+    vertex(this.size/3, this.size/7.5 - this.size / 2);
     endShape();
 
+    //cheeks
     fill(51, 136, 255);
     noStroke();
-    circle(this.size / 3.33, 7, 4);
-    circle(-9, 7, 4);
-    fill(0);
-    circle(-8, 0, 6);
-    circle(8, 0, 6);
-    circle(0, 6, 4);
+    circle(this.size / 3.33, this.size/4.83, this.size/7.5);
+    circle(this.size/-3.33, this.size/4.83, this.size/7.5);
 
+    //eyes and mouth
+    fill(0);
+    circle(this.size/-3.75, 0, this.size/5);
+    circle(this.size/3.75, 0, this.size/5);
+    circle(0, this.size/5, this.size/7.5);
+    
+    //pupils
     fill(255);
-    circle(-9, -1, 2);
-    circle(7, -1, 2);
+    circle(this.size/-3.33, this.size/-30, this.size/15);
+    circle(this.size/4.3, this.size/-30, this.size/15);
 
   }
 }
